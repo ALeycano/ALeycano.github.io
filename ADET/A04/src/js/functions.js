6038d4a46ce82922c21a9b8aa1ee3b1b06f683c6
ALeycano <leycano32@gmail.com>
2025-05-16 01:19:42 +0800

    A04 BE

--- ADET/A04/src/js/functions.js
+++ ADET/A04/src/js/functions.js
@@ -1,40 +1,71 @@
+var categories = [];
+var products = [];
+
+var maincontainer = document.getElementById("maincontainer");
 var total = 0;
+
+const getAllCategories = async () => {
+    fetch('http://localhost/Github/ALeycano.github.io/ADET/A06/categories.php')
+        .then(response => response.json())
+        .then(data => {
+            categories = data;
+            loadCategories();
+        });
+};
+
+const getAllProducts = async (categoryID) => {
+    const categoryData = { categoryID: categoryID };
+
+    fetch('http://localhost/Github/ALeycano.github.io/ADET/A06/products.php', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(categoryData)
+    })
+        .then(response => response.json())
+        .then(data => {
+            products = data;
+            loadProducts();
+        });
+};
 
 function loadCategories() {
     var categoriesContainer = document.getElementById("categories");
+    categoriesContainer.innerHTML = "";
 
-    products.forEach((product, index) => {
+    categories.forEach(function (category) {
         categoriesContainer.innerHTML +=
-            '<div onclick="loadProducts(' + index + ')" class="card mx-1 btn-card rounded p-3 text-center">' +
-            '<small>' + product.category + '</small>' +
+            '<div onclick="getAllProducts(' + category.categoryID + ')" class="card mx-1 btn-card rounded p-3 text-center">' +
+            '<small>' + category.name + '</small>' +
             '</div>';
     });
 }
 
-function loadProducts(categoryIndex) {
-    var maincontainer = document.getElementById("maincontainer");
+function loadProducts() {
     maincontainer.innerHTML = "";
 
-    products[categoryIndex].contents.forEach(content => {
+    products.forEach(function (product) {
         maincontainer.innerHTML +=
-            '<div onclick="addToReceipt(' + content.price + ', \'' + content.code + '\', \'' + content.name + '\', \'' + content.description + '\')" class="card mx-1 my-2 btn-card content p-3 text-center">' +
-            '<img src="' + content.image + '" alt="' + content.name + '" class="img-fluid mb-2" style="max-width: 100px; height: auto; display: block; margin: 0 auto;">' +
-            '<small class="name mt-3 mb-2">' + content.name + '</small>' +
-            '<div class="price">₱' + content.price + '</div>' +
-            '<small class="description text-muted mt-3">' + content.description + '</small>' +
+            '<div onclick="addToReceipt(' + product.price + ', \'' + product.code + '\', \'' + product.name + '\', \'' + product.description + '\')" class="card mx-1 my-2 btn-card content p-3 text-center">' +
+            '<img src="' + product.image + '" alt="' + product.name + '" class="img-fluid mb-2" style="max-width: 100px; height: auto; display: block; margin: 0 auto;">' +
+            '<small class="name mt-3 mb-2">' + product.name + '</small>' +
+            '<div class="price">₱' + product.price + '</div>' +
+            '<small class="description text-muted mt-3">' + product.description + '</small>' +
             '</div>';
     });
 }
 
 function addToReceipt(price, code, name, description) {
-    if (document.getElementById("receipt").innerText === "Receipt printed.") {
-        document.getElementById("receipt").innerHTML = "";
+    var receiptContainer = document.getElementById("receipt");
+
+    if (receiptContainer.innerText === "Receipt printed.") {
+        receiptContainer.innerHTML = "";
     }
 
-    var receiptContainer = document.getElementById("receipt");
-    var items = document.getElementById("receipt-item" + code);
+    var item = document.getElementById("receipt-item" + code);
 
-    if (items) {
+    if (item) {
         var quantity = document.getElementById("quantity" + code);
         var totalSpan = document.getElementById("line-total" + code);
         quantity.innerText = parseFloat(quantity.innerText) + 1;
@@ -63,4 +94,4 @@ function clearReceipt() {
     total = 0;
 }
 
-loadCategories();
+getAllCategories();
